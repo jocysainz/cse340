42bd38138582fad7f25395299fdb70d93aca552c
jocysainz <lauragrajeda29@yahoo.com>
2026-09-05 18:35:23 -0500

    fix

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('/projects', async (req, res) => {
   res.render('projects', { title });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running at http://0.0.0.0:${PORT}`);
   console.log(`Environment: ${NODE_ENV}`);
 });
